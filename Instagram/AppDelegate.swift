1c2cd5fc6fd8649514506c02853474c68ea6692f
freddieclarke13 <freddieclarke13@gmail.com>
2021-03-02 20:32:33 -0500

    working on image posting

--- Instagram/AppDelegate.swift
+++ Instagram/AppDelegate.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import Parse
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        let parseConfig = ParseClientConfiguration {
+                    $0.applicationId = "aueOwqcQERg6nVn8Da1Ruoyf9WRAV6TrotXTZLJ1" // <- UPDATE
+                    $0.clientKey = "nYbzHXlZshPCznMfBMOO3kClHRNcouo0Wx9Z5g64" // <- UPDATE
+                    $0.server = "https://parseapi.back4app.com"
+            }
+        Parse.initialize(with: parseConfig)
+        
         return true
     }
 
---
+++ Instagram/keys.md
@@ -0,0 +1,4 @@
+#  Keys
+
+App ID - aueOwqcQERg6nVn8Da1Ruoyf9WRAV6TrotXTZLJ1
+Client Key - nYbzHXlZshPCznMfBMOO3kClHRNcouo0Wx9Z5g64
